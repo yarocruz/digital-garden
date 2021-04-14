f4beb1977579cde0f8d622b22c65e586177d47ac
yarocruz <yarocruz@gmail.com>
2021-04-14 14:52:35 -0400

    added additional tests for write --help and write command

--- tests/integration.rs
+++ tests/integration.rs
@@ -9,3 +9,21 @@ fn test_help() -> Result<()> {
     assert.success().stderr("");
     Ok(())
 }
+
+#[test]
+/// Make sure we have a write command by running `garden write --help`
+fn test_write_help() -> Result<()> {
+    let mut cmd = Command::cargo_bin("garden")?;
+    let assert = cmd.arg("write").arg("--help").assert();
+    assert.success().stderr("");
+    Ok(())
+}
+
+#[test]
+#[ignore]
+/// Execute the write command saving a file out.
+fn test_write() {
+    let mut cmd = Command::cargo_bin("garden").unwrap();
+    let assert = cmd.arg("write").assert();
+    assert.success();
+}
