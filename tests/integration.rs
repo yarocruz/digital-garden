506613b629164f73f852846eb6c84488cee75f06
yarocruz <yarocruz@gmail.com>
2021-04-20 16:39:53 -0400

    finished rust course

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,5 +1,24 @@
 use assert_cmd::Command;
+use assert_fs::prelude::*;
 use color_eyre::eyre::Result;
+use predicates::prelude::*;
+
+fn setup_command() -> (Command, assert_fs::TempDir) {
+    let temp_dir = assert_fs::TempDir::new().unwrap();
+
+    let mut cmd = Command::cargo_bin("garden").unwrap();
+    let fake_editor_path = std::env::current_dir()
+        .expect("expect to be in a dir")
+        .join("tests")
+        .join("fake_editor.sh");
+    if !fake_editor_path.exists() {
+        panic!("Fake editor shell script could not be found")
+    }
+
+    cmd.env("EDITOR", fake_editor_path.into_os_string())
+        .env("GARDEN_PATH", temp_dir.path());
+    (cmd, temp_dir)
+}
 
 #[test]
 /// Make sure help runs. This indicates the binary works.
@@ -20,10 +39,37 @@ fn test_write_help() -> Result<()> {
 }
 
 #[test]
-#[ignore]
 /// Execute the write command saving a file out.
-fn test_write() {
-    let mut cmd = Command::cargo_bin("garden").unwrap();
-    let assert = cmd.arg("write").assert();
+fn test_write_with_title() {
+    let (mut cmd, temp_dir) = setup_command();
+
+    let assert = cmd
+        .arg("write")
+        .arg("-t")
+        .arg("atitle")
+        .write_stdin("N\n".as_bytes())
+        .assert();
+
     assert.success();
+
+    temp_dir
+        .child("atitle.md")
+        .assert(predicate::path::exists());
+}
+
+#[test]
+/// Execute the write command saving a file out.
+fn test_write_with_written_title() {
+    let (mut cmd, temp_dir) = setup_command();
+
+    let assert = cmd
+        .arg("write")
+        .write_stdin("N\n".as_bytes())
+        .assert();
+
+    assert.success();
+
+    temp_dir
+        .child("testing.md")
+        .assert(predicate::path::exists());
 }
